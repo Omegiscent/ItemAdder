6138da253d1d914311539d5d680fc9c9b58e17ca
Omega <omega@alpha.org>
2014-07-05 14:28:33 -0400

    CoinAdder

--- coins.rb
+++ coins.rb
@@ -6,7 +6,7 @@ require 'openssl'
 
 class Adder
    @@api = 'FD 9B407F96-418B-4E0B-93DB-3AD33CC7D72E:205EF7823B24EE5277E318E061E5557F4648F1BF4CCFB457'
-   def AddItem(username, password, items)
+   def AddCoins(username, password, coins)
       base = username + ':' + password
       base = Base64.encode64(base)
       base = base.encode('utf-8')
@@ -26,29 +26,31 @@ class Adder
             abort("Login Failed")
          end   
       end
+      
       authToken = JSON.parse(@@x.body)
       authToken = authToken['authToken']
       @@authToken=authToken
       base = @@authToken + ':'
       base = Base64.strict_encode64(base.encode('ascii'))
       base = base.encode('utf-8')
+      
       headers = {
          'Authorization' => "Basic #{base.strip}" + ',' + @@api
       }
-      for item in items
-         url = URI.parse("https://api.disney.com/clubpenguin/mobile/v2/purchase")
-         req = Net::HTTP::Post.new("/clubpenguin/mobile/v2/purchase?catalogId=500435792&itemType=paper_item&itemId=#{item}", headers)
-         res = Net::HTTP.new(url.host, url.port)
-         res.use_ssl=true
-         res.start do |http|
-            @@x = http.request(req)
-            if "#{@@x}".include? "HTTPOK"
-               puts "Item: #{item} added"
-            else
-               message = JSON.parse(@@x.body)
-               message = message["errorResponse"]["message"]
-               puts "Item was not added. Reason: #{message}"
-            end
+      
+      url = URI.parse("https://api.disney.com/clubpenguin/mobile/v2/coins?appId=CPMCAPP")
+      req = Net::HTTP::Put.new("/clubpenguin/mobile/v2/coins?appId=CPMCAPP", headers)
+      req.body = '{"$inc":{"coins":'+coins.to_s+'}}'
+      res = Net::HTTP.new(url.host, url.port)
+      res.use_ssl=true
+      res.start do |http|
+         @@x = http.request(req)
+         if "#{@@x}".include? "HTTPOK"
+            puts "#{coins} added"
+         else
+            message = JSON.parse(@@x.body)
+            message = message["errorResponse"]["message"]
+            puts "Coins were not added. Reason: #{message}"
          end
       end
    end
@@ -56,6 +58,6 @@ end
 # Penguin information
 username = 'orange2juice'
 password = 'dicks1234'
-items = [413]
-ItemAdder = Adder.new
-ItemAdder.AddItem(username, password, items)
+coins = 1000
+CoinAdder = Adder.new
+CoinAdder.AddCoins(username, password, coins)
